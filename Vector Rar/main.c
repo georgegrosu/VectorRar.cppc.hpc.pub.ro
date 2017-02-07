c9dd3cfc7f17753d46e5887e6c45d56e97535861
Grosu George <grosu.george@outlook.com>
2017-02-07 11:01:39 +0200

    Initial Commit

---
+++ Vector Rar/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Vector Rar
+//
+//  Created by George Mircea on 2/7/17.
+//  Copyright © 2017 Grosu George-Mircea. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
